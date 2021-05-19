238989e27d6aca13fcae2ff9453f718d4c29740c
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-05-19 18:32:42 +0530

    Intial Setup to start property app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Launch from './components/launch'
+
+class App extends React.Component {
+  render (){
+    return (
+      <Launch />
+    )
+  }
+}
+
+export default App;
