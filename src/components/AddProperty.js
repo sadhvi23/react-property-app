77eb55348fe0d0fc8483bdad2e8fe3a1ce06a196
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-05-28 20:01:54 +0530

    Added add owner, update, deactivate, delete property actions

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 
 import { Button } from "../launchComponents/formLayouts/buttonLayout"
 import { Input } from "../launchComponents/formLayouts/inputLayout"
-import { addProperty } from "../actions/properties";
+import { addProperty, updateProperty } from "../actions/properties";
 
 const AddProperty = (props) => {
 
@@ -24,11 +24,46 @@ const AddProperty = (props) => {
 
   const [property, setProperty] = useState(formik.values);
 
+  // Save property on form submit
   const saveProperty = async (e) => {
     e.preventDefault();
-    const { name, is_approved, is_available, is_active } = formik.values;
+    const { editMode, propertyData} = props
+    if (editMode) {
+      update(propertyData)
+    } else {
+      add()
+    }
+  };
+
+  // Add Property if edit mode is false
+  const add = () => {
+    const { name, is_approved, is_available } = formik.values;
     if (name) {
-      dispatch(addProperty(name, is_approved, is_available, is_active))
+      dispatch(addProperty(name, is_approved, is_available))
+      .then(data => { 
+        setProperty({
+          ...formik.values,
+          id: data.id,
+          name: data.name,
+          is_approved: data.is_approved,
+          is_available: data.is_available
+        });
+        localStorage.setItem('propertyId', data.id)
+        props.history.push("/properties")
+        setProperty({...property, name:'', message: data.name + " has been added successfully" })
+      })
+      .catch(e => {
+        console.log(e.message);
+        setProperty({...property, message: e.message})
+      });
+    }
+  }
+
+  // Update property if edit mode is true
+  const update = (propertyData) => {
+    const { name, is_approved, is_available } = formik.values;
+    if (name) {
+      dispatch(updateProperty(propertyData.id, name, is_approved, is_available))
       .then(data => { 
         setProperty({
           ...props.formik.values,
@@ -40,18 +75,18 @@ const AddProperty = (props) => {
         });
         localStorage.setItem('propertyId', data.id)
         props.history.push("/properties")
-        setProperty({...property, name:'', message: "Request has been processed successfully" })
+        setProperty({...property, name:'', message: data.name + " has been updated successfully" })
       })
       .catch(e => {
         console.log(e.message);
         setProperty({...property, message: e.message})
       });
     }
-  };
+  }
 
   return (
     <form onSubmit={saveProperty}>
-    <h3>Add Property</h3>
+    {props.editMode ? (<h3>Update Property</h3>) : (<h3>Add Property</h3>)}
 
     <Input divClass="form-group" label="Name" type="name" name="name" class="form-control" placeholder=
     "Enter name" value={formik.values.name} handleChange={formik.handleChange}/>
@@ -66,14 +101,17 @@ const AddProperty = (props) => {
     <Input divClass="form-group" label="false" type="radio" name="is_available" handleChange={formik.handleChange} value={formik.values.is_available} />   
     <br />
 
-    <label>IsActive</label>
-    <Input divClass="form-group" label="true" type="radio" name="is_active" handleChange={formik.handleChange} value={formik.values.is_active} />
-    <Input divClass="form-group" label="false" type="radio" name="is_active" handleChange={formik.handleChange} value={formik.values.is_active} />   
-    <br />
-
-    <Button type="submit" class="btn btn-dark btn-lg btn-block" disabled={!(formik.values.name)} label="AddProperty" />
-  </form>
+    {props.editMode ? (
+      <Button type="submit" class="btn btn-dark btn-lg btn-block" disabled={!(formik.values.name)} label="UpdateProperty" />
+    ) : (
+      <Button type="submit" class="btn btn-dark btn-lg btn-block" disabled={!(formik.values.name)} label="AddProperty" />
+    )}
+    </form>
   );
+}
+
+AddProperty.defaultProps = {
+  editMode: false,    // false: Create mode, true: Edit mode
 }
 
 export default withRouter(AddProperty);
--- src/services/propertyService.js
+++ src/services/propertyService.js
@@ -8,6 +8,32 @@ const listProperty = () => {
   return http.get("/properties", { headers: { Authorization: "Bearer " + localStorage.token } });
 };
 
+const deactivateProperty = (data) => {
+  return http.put("/properties/"+ data.id + "/deactivate", data, { headers: { Authorization: "Bearer " + localStorage.token }});
+};
+
+const updateProperty = (data) => {
+  return http.put("/properties/"+ data.id, data, { headers: { Authorization: "Bearer " + localStorage.token }});
+};
+
+const deleteProperty = (data) => {
+  return http.delete("/properties/"+ data.id, { headers: { Authorization: "Bearer " + localStorage.token }});
+};
+
+const buyProperty = (data) => {
+  return http.put("/properties/"+ data.id + "/availability", data, { headers: { Authorization: "Bearer " + localStorage.token }});
+};
+
+const addOwner = (data) => {
+  return http.post("/properties/"+ data.id + "/add_owner", data, { headers: { Authorization: "Bearer " + localStorage.token }});
+};
+
+const MyProperties = () => {
+  return http.get("/properties/me", { headers: { Authorization: "Bearer " + localStorage.token } });
+};
+
+
 export const propertyService = {
-    addProperty, listProperty
+    addProperty, listProperty, deactivateProperty, updateProperty, deleteProperty, buyProperty, 
+    addOwner, MyProperties
 };
