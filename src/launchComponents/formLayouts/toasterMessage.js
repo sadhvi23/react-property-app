b9e8d86fc102e589e77499af8f044d904289b3a0
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-05-31 19:28:02 +0530

    Removed blank toaster from login and signup page

--- src/launchComponents/formLayouts/toasterMessage.js
+++
@@ -1,9 +0,0 @@
-import Toast from 'react-bootstrap/Toast'
-
-export const ToasterMessage = (props) => {    
-    return (
-        <Toast>
-            <Toast.Body>{props.message}</Toast.Body>
-        </Toast>
-    );
-  }
